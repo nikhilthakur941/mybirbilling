041b5ff6a8528a79b253ef34b168ba2e89d39bb7
nikhilthakur941 <tnikhil084@gmail.com>
2026-05-12 02:00:35 +0530

    fix prisma tables

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -1,9 +1,19 @@
 import { v2 as cloudinary } from "cloudinary";
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
-});
+export function getCloudinary() {
+  const { CLOUDINARY_NAME, CLOUDINARY_KEY, CLOUDINARY_SECRET } = process.env;
+
+  if (!CLOUDINARY_NAME || !CLOUDINARY_KEY || !CLOUDINARY_SECRET) {
+    throw new Error("❌ Missing Cloudinary environment variables");
+  }
+
+  cloudinary.config({
+    cloud_name: CLOUDINARY_NAME,
+    api_key: CLOUDINARY_KEY,
+    api_secret: CLOUDINARY_SECRET,
+  });
+
+  return cloudinary;
+}
 
 export default cloudinary;
